dc9d09550e01f59a4709f35c4543d99e32e74443
Jose Daniel <jose.grijalba04@gmail.com>
2025-09-18 00:25:30 -0500

    .

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,7 @@ use App\Models\Post;
 use App\Notifications\PostNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
